c58fba803dea2615d4fe0feaa1bb5f79a128a71a
Whoistolu <digittolu25@gmail.com>
2026-04-11 08:14:39 +0100

    Add Restaurant model with name and slug index

---
+++ db/migrate/20260411070812_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[8.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :slug
+
+      t.timestamps
+    end
+    add_index :restaurants, :slug
+  end
+end
